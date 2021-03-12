71a1a558f5fba69939d2e74467e09b80e143c619
EpicViv <70602891+VivekIly@users.noreply.github.com>
2021-03-12 10:30:20 -0600

    Finished 3/12 notes on subclasses and inheritance.

---
+++ FillInQuestion.java
@@ -0,0 +1,29 @@
+/*
+
+The FillInQuestino class extends (i.e., is a subclass of) the Question class.
+
+ */
+public class FillInQuestion extends Question {
+    /*
+
+    Do not declare instance variables for text and answer! The text and answer instance variables are inherited from the Question class.
+
+     */
+
+    /**
+     * Constructs a FillInQuestion object with the specified text that contains the answer within.
+     * @param question
+     */
+    public FillInQuestion(String question) {
+
+    }
+
+    /*
+
+    Use the @Override annotation when overriding a method to help the compiler verify that you in fact overiding and not overloading by mistake.
+     */
+    @Override
+    public void setText(String questionText) {
+
+    }
+}
